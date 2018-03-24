6e70447d173e84dd581c505d1744124cf23e4bba
Bart Steverink <bart.steverink@gmail.com>
2018-03-24 18:43:05 +0100

    JENKINS: made sure the build tag is used

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -2,6 +2,9 @@
 
 echo 'Deploying django-docker image with tag: '$1
 
+echo 'Replace the latest tag with the current build tag in the docker-compose file'
+sed -i 's/demo:latest/demo:${BUILD_NUMBER}/' rancher/docker-compose.yml
+
 echo 'Making sure the webapp is already running'$1
 rancher-compose -f rancher/docker-compose.yml -r rancher/rancher-compose.yml -p docker-demo up -c -d webapp
 

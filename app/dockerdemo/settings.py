b6ebb8a4020e4eedd47ac5a9ae1fb56ac09b6ef5
Bart Steverink <bart.steverink@gmail.com>
2018-03-27 21:28:44 +0200

    debug uit

--- app/dockerdemo/settings.py
+++ app/dockerdemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9$chm!n2*-=&t*tb#s=gg)6q5ort@phseyob=3tu-=3z$6#l%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
